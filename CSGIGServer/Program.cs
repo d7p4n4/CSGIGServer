05711b66520d393d7def6e135ff9bda40d52247c
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-17 12:41:22 +0200

    1.20200417.2

---
+++ CSGIGServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSGIGServer
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            GetUserFromByTokenResponse getUserFromByTokenResponse =
+                new GigServerPersistentObjectService().GetUserFromByToken(new GetUserFromByTokenReqest()
+                {
+                    fbToken = "123456"
+                });
+
+            Console.WriteLine(getUserFromByTokenResponse);
+        }
+    }
+}
